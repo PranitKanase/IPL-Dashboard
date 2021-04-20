1d7802fbea26e2fa75d9e2e0ddbbfbc087ad4765
Pranit Kanase <kanasepranit2@gmail.com>
2021-04-20 18:52:16 +0530

    React app with team page

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,32 +1,56 @@
 import { React, useEffect, useState } from 'react';
+import {useParams} from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
+import { PieChart } from 'react-minimal-pie-chart';
+
+import './TeamPage.scss';
 
 export const TeamPage = () =>{
 
     const [team, setTeam] = useState({matches : []});
+    const { teamName } = useParams();
 
- useEffect(
+      useEffect(
 
-    () => {
-        const fetchMatches = async() => {
-            const response = await fetch('http://localhost:8080/team/Chennai%20Super%20Kings');
-            const data = await response.json();
-            setTeam(data);
- 
-        };
-        fetchMatches();
-    }, []
- );
+          () => {
+              const fetchMatches = async() => {
+                  const response = await fetch(`http://localhost:8080/team/${teamName}`);
+                  const data = await response.json();
+                  setTeam(data);
+      
+              };
+              fetchMatches();
+          }, [teamName]
+        );
 
- return (
-    <div className="TeamPage">
-      <h1>{team.teamName}</h1>
+        if( !team || !team.teamName) {
+          return <h1>Team not found</h1>
+        }
 
-      <MatchDetailCard match = { team.matches[0] } />
-      {team.matches.slice(1).map(match => (<MatchSmallCard match= {match}/>))}
-
-    </div>
-  );
+        return (
+            <div className="TeamPage">
+              <div className="team-name-section">
+                <h1 className="team-name">{team.teamName}</h1>
+              </div>
+              <div className="win-loss-section">
+                Wins / Losses
+                <PieChart 
+                  data={[
+                    {title:'Losses', value:team.totalMatches - team.totalWins, color: '#a34d5d'},
+                    {title:'Wins', value:team.totalWins, color: '#4da375'}
+                  ]}
+                />
+              </div>
+              <div className = "match-deatil-section">
+                <h3>Latest Matches</h3>
+                <MatchDetailCard teamName = {team.teamName} match = { team.matches[0] } />
+              </div>
+              {team.matches.slice(1).map(match => (<MatchSmallCard teamName = {team.teamName} match= {match}/>))}
+              <div className="more-link">
+                <a href="#">More ></a>
+              </div>
+            </div>
+          );
 }
 
--- src/main/java/com/learn/ipldashboard/controller/TeamController.java
+++ src/main/java/com/learn/ipldashboard/controller/TeamController.java
@@ -1,5 +1,9 @@
 package com.learn.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.learn.ipldashboard.model.Match;
 import com.learn.ipldashboard.model.Team;
 import com.learn.ipldashboard.repository.MatchRepository;
 import com.learn.ipldashboard.repository.TeamRepository;
@@ -8,6 +12,7 @@ import com.learn.ipldashboard.repository.TeamRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,4 +39,15 @@ public class TeamController {
 
         return team;
     }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year){
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return this.matchRepository.getMatchesByTeamBetweenDates(
+            teamName,
+            startDate,
+            endDate
+            );
+    } 
 }
